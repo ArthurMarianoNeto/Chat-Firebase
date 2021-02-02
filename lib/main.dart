b9d6cd757b771383a7405a4d3f36582f8d0b0182
ArthurMarianoNeto <thiesto@gmail.com>
2021-02-02 10:46:21 -0300

    Teste de acesso das mensagens já gravadas no banco de dados Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 //  Firestore.instance.collection("col").document("doc").setData({"texto":"Arthur"});
-    Firestore.instance.collection("mensagens").document("msg2").setData({
-      "texto": "Estou bem demais",
-      "from":  "Joaquina",
+/*    Firestore.instance.collection("mensagens").document().setData({
+      "texto": "Testando chave aleatoria",
+      "from":  "Jacira",
       "read": false
     });
+*/
+  QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+  snapshot.documents.forEach((d){
+  print(d.data);
+  });
 }
 class MyApp extends StatelessWidget {
   @override
