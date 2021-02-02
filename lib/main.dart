1dfb1927fbb5a491b1df20f933e4721ca0d3a666
ArthurMarianoNeto <thiesto@gmail.com>
2021-02-01 21:11:53 -0300

    Resolvido o problema setings.gradle Realizado testes em banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
-  Firestore.instance.collection("col").document("doc").setData({"texto":"Arthur"});
+//  Firestore.instance.collection("col").document("doc").setData({"texto":"Arthur"});
+    Firestore.instance.collection("mensagens").document("msg2").setData({
+      "texto": "Estou bem demais",
+      "from":  "Joaquina",
+      "read": false
+    });
 }
 class MyApp extends StatelessWidget {
   @override
